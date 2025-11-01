816ab346688670fdb9558369a9ca99582bb46e77
= <gohelbhargav401@gmail.com>
2025-11-01 19:55:37 +0530

    New SkillsPages are updated

--- src/pages/SkillsPage.jsx
+++ src/pages/SkillsPage.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import NavBar from '../components/NavBar'
+import SkillsSection from '../components/SkillsSection'
 
 function SkillsPage() {
+
   return (
-    <div>
-        <p>Hello From Skills</p>
+    <div className='bg-slate-800 bg-linear-to-b from-slate-800 to-black'>
+        <SkillsSection />
     </div>
   )
 }
