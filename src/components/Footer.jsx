f67191c688c2d0c6c96d67cf8204531c9aad64f3
= <gohelbhargav401@gmail.com>
2025-11-02 13:35:42 +0530

    footer section is updated

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from "react";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <div>
+        <footer className="text-white py-10 text-center bg-linear-to-b from-black to-slate-950 font-mono font-bold">
+          <h2 className="text-3xl p-3 font-semibold mb-4">
+            My Contributions....
+          </h2>
+          <img
+            src={`https://ghchart.rshah.org/GohelBhargav13`}
+            alt="Bhargav's GitHub chart"
+            className="mx-auto w-full max-w-4xl"
+          />
+          <p className="text-gray-400 mt-4">
+            © { new Date().getFullYear() } Built by Bhargav 🚀 | #BuiltByBhargav
+          </p>
+        </footer>
+      </div>
+  );
+};
 
-export default Footer
+export default Footer;
