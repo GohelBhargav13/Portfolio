ef24276413b6130a0ca31edac4d206c26c7b976f
= <gohelbhargav401@gmail.com>
2025-11-09 12:45:51 +0530

    home page UI updated

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,5 +1,5 @@
 import CodeSection from "../components/CodeSection";
-import profileImage from "../assets/Bhargav_Image.jpg"
+
 
 const HomePage = () => {
   const profileData = {
@@ -18,7 +18,7 @@ const HomePage = () => {
       <div className="flex flex-col md:flex-col lg:flex-row items-center justify-center gap-12 md:gap-20 lg:gap-32">
         {/* Left Section - Profile Info */}
         <div className="bg-linear-to-b from-gray-800 to-gray-900 hover:from-gray-700 hover:to-black rounded-xl p-5 transition-transform duration-300 hover:scale-105 shadow-lg shadow-black/70 max-w-lg 
-        w-full mt-4 sm:mt-4 sm:w-[400px] md:w-[460px] lg:w-[500px]">
+        w-full sm:mt-4 sm:w-[400px] md:w-[460px] lg:w-[500px]">
           <div className="flex items-center text-sm md:text-base lg:text-lg gap-2">
         
             <p className="font-bold text-orange-600 font-mono">const</p>
