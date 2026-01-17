87d0ce36fc20f4ee357d471c079314291e79ea19
= <gohelbhargav401@gmail.com>
2026-01-18 02:02:55 +0530

    fix: update a UI of project section

--- src/components/ProjectSection.jsx
+++ src/components/ProjectSection.jsx
@@ -1,7 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { ChevronDown,ChevronUp } from "lucide-react"
 
 const ProjectSection = () => {
   const Projects = [
+    { 
+      title: "DevHub",
+      techStack: "[ 'ReactJs', 'ExpressJs', 'Postgresql', 'Socket.io', 'drizzle', 'JWT', 'TailwindCSS' ]", 
+      description: "A platform where devs can share a latest tech news, tech updates with tech community.", 
+      link: "https://github.com/GohelBhargav13/Devhub",
+      functions: `{ 
+        createPost : 'create a fresh tech post',
+        savePost: 'users can save a posts'
+        Q&A: 'A time bound question&Answer section where devs can ask a quesions to the community'
+        deleteAccount:'A new feature of user account deletion',
+        RT-Comment:'A real-time comment system on the Q&A',
+        adminPanel : 'Admin can manage posts and users'
+      }`,
+      tools: "['Git', 'GitHub', 'Postman API','docker']",
+      live_on:"https://devhub-self.vercel.app/"
+    },
     { 
       title: "TubeX",
       techStack: "[ 'ReactJs', 'ExpressJs', 'MongoDB', 'Socket.io', 'mongoose', 'JWT', 'ffmpeg', 'AWS S3', 'TailwindCSS' ]", 
@@ -56,74 +73,100 @@ const ProjectSection = () => {
     }
   ];
 
+  // state of the show button
+  const [isshow,setIsShow] = useState(false)
+  const [projectIndex,setIsProjectIndex] = useState(null)
+  const [projectLimit,setIsProjectLimit] = useState(2)
+
+  // handle the show toggle
+  const handleToggelShow = (projectIndex) => {
+    setIsProjectIndex(prev => prev === projectIndex ? null : projectIndex)
+    setIsShow(prev => projectIndex !== null ? true : false)
+    console.log(projectIndex)
+  }
+
+  // handle the change a limit of the project show
+  const handleChangeProjectLimit = () => {
+    setIsProjectLimit(prev => prev === Projects.length ? 2 : Projects.length)
+  }
+
   return (
     <div className="flex flex-col items-center justify-center p-3 mt-5">
-      {Projects.map((project, idx) => (
-        <div key={idx} className="bg-slate-800 text-gray-200 font-mono rounded-2xl shadow-lg shadow-black/40 
-            w-[90%] sm:w-[85%] md:w-[80%] lg:w-[70%] xl:w-[60%] 
-            p-4 sm:p-6 md:p-7 m-3 hover:scale-[1.02] transition-transform duration-300 ease-in-out">
-          
-          <p className="text-orange-400 font-bold mb-2 text-sm sm:text-base md:text-lg">
-            {idx}: {"{"}
-          </p>
+      { Projects.map((project, idx) => (
+        <>
+          { idx < projectLimit ? (
+            <div key={idx} className="bg-slate-800 text-gray-200 font-mono rounded-2xl shadow-lg shadow-black/40 
+              w-[90%] sm:w-[85%] md:w-[80%] lg:w-[70%] xl:w-[60%] 
+              p-4 sm:p-6 md:p-7 m-3 hover:scale-[1.02] transition-transform duration-300 ease-in-out">
+            { isshow && idx === projectIndex ? <ChevronUp className='p-0.5 m-2 cursor-pointer bg-slate-950/40 rounded-xl border-2 border-gray-500 hover:bg-slate-900/20' onClick={() => handleToggelShow(idx)} />  : <ChevronDown className='p-0.5 m-2 cursor-pointer bg-slate-950/40 rounded-xl border-2 border-gray-500 hover:bg-slate-900/20' onClick={() => handleToggelShow(idx)}/>
+            } 
+            <p className="text-orange-400 font-bold mb-2 text-sm sm:text-base md:text-lg">
+              {idx}: {"{"}
+            </p>
 
-          <div className="ml-3 space-y-3">
-            <div className="flex flex-wrap gap-2">
-              <h1 className="text-yellow-300 font-bold">title:</h1>
-              <p className="text-green-400">"{project.title}" ,</p>
-            </div>
-
-            <div className="flex flex-wrap gap-2">
-              <h1 className="text-yellow-300 font-bold">description:</h1>
-              <p className="text-green-400">"{project.description}" ,</p>
-            </div>
-
-            <div className="flex flex-wrap gap-2">
-              <h1 className="text-yellow-300 font-bold">techStack:</h1>
-              <p className="text-green-400 wrap-break-word">"{project.techStack}" ,</p>
-            </div>
-
-            <div>
-              <h1 className="text-yellow-300 font-bold mb-1">features:</h1>
-              <pre className="text-green-400 text-sm sm:text-base whitespace-pre-wrap overflow-x-auto">
-                "{project.functions}"
-              </pre>
-            </div>
-
-            <div className="flex flex-wrap gap-2">
-              <h1 className="text-yellow-300 font-bold">tools:</h1>
-              <p className="text-green-400 wrap-break-word">"{project.tools}" ,</p>
-            </div>
-
-            <div className="flex flex-wrap gap-2">
-              <h1 className="text-yellow-300 font-bold">github:</h1>
-              <a
-                href={project.link}
-                target="_blank"
-                rel="noopener noreferrer"
-                className="text-blue-400 underline hover:text-blue-300"
-              >
-                "{project.link}"
-              </a>
-            </div>
-            { project.live_on && (
+            <div className="ml-3 space-y-3">
               <div className="flex flex-wrap gap-2">
-              <h1 className="text-yellow-300 font-bold">Live on:</h1>
-              <a
-                href={project?.live_on}
-                target="_blank"
-                rel="noopener noreferrer"
-                className="text-blue-400 underline hover:text-blue-300"
-              >
-                "{project?.live_on}"
-              </a>
-            </div>
-            ) }
-          </div>
+                <h1 className="text-yellow-300 font-bold">title:</h1>
+                <p className="text-green-400">"{project.title}" ,</p>
+              </div>
 
-          <p className="font-bold text-orange-400 mt-2 text-sm sm:text-base md:text-lg">{"},"}</p>
-        </div>
+              <div className="flex flex-wrap gap-2">
+                <h1 className="text-yellow-300 font-bold">description:</h1>
+                <p className="text-green-400">"{project.description}" ,</p>
+              </div>
+
+              <div className="flex flex-wrap gap-2">
+                <h1 className="text-yellow-300 font-bold">techStack:</h1>
+                <p className="text-green-400 wrap-break-word">"{project.techStack}" ,</p>
+              </div>
+
+              { isshow && projectIndex === idx ? (
+                <div className={`${isshow ? "transform transition-transform duration-300 rotate-2" : ""}`}>
+                  <div>
+                    <h1 className="text-yellow-300 font-bold mb-1">features:</h1>
+                    <pre className="text-green-400 text-sm sm:text-base whitespace-pre-wrap overflow-x-auto">
+                      "{project.functions}"
+                    </pre>
+                  </div>
+
+                  <div className="flex flex-wrap gap-2">
+                    <h1 className="text-yellow-300 font-bold">tools:</h1>
+                    <p className="text-green-400 wrap-break-word">"{project.tools}" ,</p>
+                  </div>
+
+                  <div className="flex flex-wrap gap-2 sm:text-wrap">
+                    <h1 className="text-yellow-300 font-bold">github:</h1>
+                    <a
+                      href={project.link}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      className="text-blue-400 underline overflow-hidden hover:text-blue-300"
+                    >
+                      "{project.link}"
+                    </a>
+                  </div>
+                  { project.live_on && (
+                    <div className="flex flex-wrap gap-2 sm:text-wrap">
+                    <h1 className="text-yellow-300 font-bold">Live on:</h1>
+                    <a
+                      href={project?.live_on}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      className="text-blue-400 underline text-wrap hover:text-blue-300"
+                    >
+                      "{project?.live_on}"
+                    </a>
+                  </div>
+                  ) }
+                </div>
+              ) : <div className='text-[18px] font-bold text-black/50 animate-pulse'>Want to explore more? Toggle to view full project details.</div>}
+            </div>
+            <p className="font-bold text-orange-400 mt-2 text-sm sm:text-base md:text-lg">{"},"}</p>
+          </div>
+          ) : <></> }
+        </>
       ))}
+      { <button className='bg-slate-950/80 p-2 rounded-lg text-blue-600/90 border-r-4 border-r-slate-600/80 border-b-4 border-b-slate-600/80 border-t-2 border-t-slate-600/80 border-l-2 border-l-slate-600/80 hover:border-r-2 hover:border-r-slate-600/80 hover:border-b-2 hover:border-b-slate-600/80 hover:duration-300 hover:ease-in cursor-pointer' onClick={handleChangeProjectLimit}>{ `${Projects.length === projectLimit ? "wrap up" : `show ${Projects.length - projectLimit}+`}` }</button> }
     </div>
   )
 }
--- src/pages/ProjectPage.jsx
+++ src/pages/ProjectPage.jsx
@@ -1,7 +1,19 @@
-import React from 'react'
+import { useState } from 'react'
 import ProjectSection from '../components/ProjectSection'
+import { Loader2 } from 'lucide-react'
 
 const ProjectPage = () => {
+  const [isProjectShow,setIsProjectShow] = useState(false)
+  const [isFetchingProject,setIsFetchingProject] = useState(false)
+
+  // handle a getting a projects
+  const handleGettingProject = () => {
+    setIsFetchingProject(true)
+      setTimeout(() => {
+        setIsProjectShow(true)
+        setIsFetchingProject(false)
+      },2000)
+  }
   return (
     <div>
         <div className='bg-linear-to-b from-slate-800 to-black font-mono text-white p-2'>
@@ -26,12 +38,12 @@ const ProjectPage = () => {
                         <h1 className='text-orange-500 font-bold'>await </h1>
                         <div className='flex'>
                         <h1 className='text-pink-600 font-bold'>fetch</h1>
-                        <h1 className='text-yellow-600 font-bold flex gap-0.5'>(<p className='text-green-500'>`http://api.github.com/GohelBhargav13/projects`,</p>{"{"}</h1>
+                        <h1 className='text-yellow-600 font-bold flex gap-0.5'>(<p className='text-green-500'>`https://api.github.com/GohelBhargav13/projects`,</p>{"["}</h1>
                         </div>
                   </div>
-                  <ProjectSection />
+                  { isProjectShow ? <ProjectSection /> : <button className={`flex mx-auto mt-15 text-[24px] m-8 p-2 rounded-xl font-bold  border-r-4 border-r-white border-b-4 border-b-white border-t-2 border-white border-l-2 ${ isFetchingProject ? "bg-red-400/60 cursor-not-allowed" : "bg-red-500/40  hover:border-r-2 hover:border-b-2 hover:border-r-white hover:border-b-white hover:duration-500 hover:ease-in cursor-pointer"}`} onClick={handleGettingProject}>{ isFetchingProject ? <Loader2 className='font-bold w-full h-full justify-items-center animate-spin' /> : "GET /projects" }</button> }
                   <div className='p-2 m-2'>
-                    <p className='text-yellow-600 font-bold text-[14px] sm:text-[16px] md:text-[19px] lg:[21px] ml-25 md:ml-28 lg:ml-30'>{"}"} )</p>
+                    <p className='text-yellow-600 font-bold text-[14px] sm:text-[16px] md:text-[19px] lg:[21px] ml-25 md:ml-28 lg:ml-30'>{"]"} )</p>
                     <p className='text-pink-400 font-bold text-[15px] sm:text-[16px] md:text-[19px] lg:text-[21px] ml-6'>{ " }" }</p>
                     <div className='p-1 m-1 text-[15px] sm:text-[16px] md:text-[19px] lg:[text-21px]'>
                         <h1 className='text-pink-400 font-bold'>catch (error) { "{ " }</h1>
